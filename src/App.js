c20a6e728086a52de6538aa564bdd511a146ec25
MadCodes9 <ceiaram@cpp.edu>
2023-05-07 20:13:41 -0700

    Declared winner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+// import { status } from "react";
 
+//Remember what square got clicked to be filled with value of 'X' or 'O'
 function Square({ value, onSquareClick }) {
-  //Remember what tile got clicked to be filled with 'X'
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -11,7 +12,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
   // 'X' or 'O' is marked based on the turn
   const [xIsNext, setXIsNext] = useState(true);
 
@@ -20,8 +21,8 @@ export default function Board() {
 
   // Update the corresponding squares array with the board's state
   function handleClick(i) {
-    //If square is already filled, return to avoid overwritting square
-    if (squares[i]) {
+    //If square is already filled OR there is a winner, return to avoid overwritting square
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -36,8 +37,18 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  // Let the player know that the game is over and who is the winner
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         {/* Only call 'handleClick when user presses square */}
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -55,5 +66,44 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+
+  //Show when the game is won and there are no more turns to make
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+}
+
+// Display a list of past moves
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
