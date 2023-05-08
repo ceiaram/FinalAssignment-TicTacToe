738885d7d294d3680f689433512ac76629428745
MadCodes9 <ceiaram@cpp.edu>
2023-05-07 19:58:08 -0700

    Added taking turn functionality

--- src/App.js
+++ src/App.js
@@ -12,20 +12,34 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  // Know state of each tile to check for winner
+  // 'X' or 'O' is marked based on the turn
+  const [xIsNext, setXIsNext] = useState(true);
+
+  // Know state of each square to check for winner
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   // Update the corresponding squares array with the board's state
   function handleClick(i) {
+    //If square is already filled, return to avoid overwritting square
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    // Based on turn, square will be filled with either 'X' or 'O'
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
       <div className="board-row">
-        {/* Only call 'handleClick when user presses tile */}
+        {/* Only call 'handleClick when user presses square */}
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
